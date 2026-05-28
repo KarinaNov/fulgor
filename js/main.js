8cc453b3871cf5af2389db94e98b443e4eea8787
ksn <ksn@evogence.com>
2026-05-28 16:22:52 +0300

    feat: add video and cta sections

--- js/main.js
+++ js/main.js
@@ -7,3 +7,31 @@ burger?.addEventListener('click', () => {
   burger.setAttribute('aria-expanded', String(!isOpen));
   nav.classList.toggle('is-open', !isOpen);
 });
+
+//  Video play button
+const videoWrapper = document.querySelector('.video__wrapper');
+const videoPlayer  = document.querySelector('.video__player');
+const videoPlayBtn = document.querySelector('.video__play');
+
+videoPlayBtn?.addEventListener('click', () => {
+  videoWrapper.classList.add('is-playing');
+  videoPlayer.setAttribute('controls', '');
+
+  const playPromise = videoPlayer.play();
+
+  if (playPromise !== undefined) {
+    playPromise.catch(() => {
+      videoWrapper.classList.remove('is-playing');
+      videoPlayer.removeAttribute('controls');
+    });
+  }
+});
+
+videoPlayer?.addEventListener('pause', () => {
+  videoWrapper.classList.remove('is-playing');
+});
+
+videoPlayer?.addEventListener('ended', () => {
+  videoWrapper.classList.remove('is-playing');
+  videoPlayer.removeAttribute('controls');
+});
